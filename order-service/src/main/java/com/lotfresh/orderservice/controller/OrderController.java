b739c203a4216b81c23ac265612b01a17466cad2
최창효 <qwerty1434@naver.com>
2023-09-25 20:11:23 +0900

    :sparkles: 주문 생성

--- order-service/src/main/java/com/lotfresh/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/lotfresh/orderservice/controller/OrderController.java
@@ -1,18 +1,23 @@
 package com.lotfresh.orderservice.controller;
 
+import com.lotfresh.orderservice.dto.OrderCreateRequest;
 import com.lotfresh.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/order")
 public class OrderController {
     private final OrderService orderService;
 
-    @PostMapping()
-    public ResponseEntity insertOrder() {
+    @PostMapping
+    public ResponseEntity insertOrder(@RequestBody OrderCreateRequest orderCreateRequest) {
+        orderService.insertOrder(orderCreateRequest);
         return ResponseEntity.ok().build();
     }
 
