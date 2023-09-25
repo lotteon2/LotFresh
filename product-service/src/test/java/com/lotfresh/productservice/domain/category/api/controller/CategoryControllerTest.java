f72e8ff3ae2f92e3ab9ccd0d28814ac3a9995b75
JunYong Sun <ssjy4974@naver.com>
2023-09-25 11:26:36 +0900

    test : createCategoryWithEmptyCategoryName test

--- product-service/src/test/java/com/lotfresh/productservice/domain/category/api/controller/CategoryControllerTest.java
+++ product-service/src/test/java/com/lotfresh/productservice/domain/category/api/controller/CategoryControllerTest.java
@@ -13,31 +13,46 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = {CategoryController.class})
 class CategoryControllerTest {
-    @Autowired
-    MockMvc mockMvc;
+  @Autowired MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
+  @Autowired ObjectMapper objectMapper;
 
-    @MockBean
-    CategoryService categoryService;
+  @MockBean CategoryService categoryService;
 
-    @DisplayName("카테고리를 생성한다.")
-    @Test
-    void test() throws Exception{
-        // given
-        CategoryCreateRequest request = new CategoryCreateRequest(null, "냉장");
-        // when // then
-        mockMvc.perform(
-                post("/categories")
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        )
-                .andDo(print())
-                .andExpect(status().isCreated());
-    }
+  @DisplayName("카테고리를 등록한다.")
+  @Test
+  void createCategory() throws Exception {
+    // given
+    CategoryCreateRequest request = new CategoryCreateRequest(null, "냉장");
+    // when // then
+    mockMvc
+        .perform(
+            post("/categories")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isCreated());
+  }
+
+  @DisplayName("카테고리 등록 시 반드시 카테고리 이름이 존재해야 한다.")
+  @Test
+  void createCategoryWithEmptyCategoryName() throws Exception {
+    // given
+    CategoryCreateRequest request = new CategoryCreateRequest(null, "");
+    // when // then
+    mockMvc
+        .perform(
+            post("/categories")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+        .andExpect(jsonPath("$.validation.name").value("name cannot be null"));
+  }
 }
