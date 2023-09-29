4f6106663653b74ffe24e5c6ff957360d4eb4b06
JunYong Sun <ssjy4974@naver.com>
2023-09-29 16:20:43 +0900

    style : formatting

--- product-service/src/test/java/com/lotfresh/productservice/ControllerTestSupport.java
+++ product-service/src/test/java/com/lotfresh/productservice/ControllerTestSupport.java
@@ -2,7 +2,6 @@ package com.lotfresh.productservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lotfresh.productservice.domain.category.api.CategoryApiController;
-import com.lotfresh.productservice.domain.category.entity.Category;
 import com.lotfresh.productservice.domain.category.service.CategoryService;
 import com.lotfresh.productservice.domain.discount.api.DiscountApiController;
 import com.lotfresh.productservice.domain.discount.service.DiscountService;
@@ -11,21 +10,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {
-        CategoryApiController.class,
-        DiscountApiController.class
-})
+@WebMvcTest(controllers = {CategoryApiController.class, DiscountApiController.class})
 public class ControllerTestSupport {
 
-    @Autowired
-    protected MockMvc mockMvc;
+  @Autowired protected MockMvc mockMvc;
 
-    @Autowired
-    protected ObjectMapper objectMapper;
+  @Autowired protected ObjectMapper objectMapper;
 
-    @MockBean
-    protected CategoryService categoryService;
+  @MockBean protected CategoryService categoryService;
 
-    @MockBean
-    protected DiscountService discountService;
+  @MockBean protected DiscountService discountService;
 }
