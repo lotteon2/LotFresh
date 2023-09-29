4f8e63cc11cf9a7cc06907c5cba27c41855502ce
JunYong Sun <ssjy4974@naver.com>
2023-09-29 15:33:37 +0900

    feat : DiscountApiController -> createDiscount

---
+++ product-service/src/main/java/com/lotfresh/productservice/domain/discount/api/DiscountApiController.java
@@ -0,0 +1,24 @@
+package com.lotfresh.productservice.domain.discount.api;
+
+import com.lotfresh.productservice.domain.discount.api.request.DiscountCreateRequest;
+import com.lotfresh.productservice.domain.discount.service.DiscountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/discounts")
+public class DiscountApiController {
+    private final DiscountService discountService;
+
+    @PostMapping("")
+    public ResponseEntity<Long> createDiscount(@Valid @RequestBody DiscountCreateRequest request) {
+        return ResponseEntity.ok(discountService.createDiscount(request));
+    }
+}
