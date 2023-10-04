de3739643bbb6b71ebd14c05d786db3111b13044
JunYong Sun <ssjy4974@naver.com>
2023-10-04 13:55:46 +0900

    test : modifyProduct service test

--- product-service/src/test/java/com/lotfresh/productservice/domain/product/service/ProductServiceTest.java
+++ product-service/src/test/java/com/lotfresh/productservice/domain/product/service/ProductServiceTest.java
@@ -4,6 +4,9 @@ import com.lotfresh.productservice.domain.category.entity.Category;
 import com.lotfresh.productservice.domain.category.exception.CategoryNotFound;
 import com.lotfresh.productservice.domain.category.repository.CategoryRepository;
 import com.lotfresh.productservice.domain.product.api.request.ProductCreateRequest;
+import com.lotfresh.productservice.domain.product.api.request.ProductModifyRequest;
+import com.lotfresh.productservice.domain.product.entity.Product;
+import com.lotfresh.productservice.domain.product.exception.ProductNotFound;
 import com.lotfresh.productservice.domain.product.repository.ProductRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -60,6 +63,94 @@ class ProductServiceTest {
     assertThatThrownBy(() -> productService.createProduct(request))
         .isInstanceOf(CategoryNotFound.class)
         .hasMessage("해당 카테고리가 존재하지 않습니다.");
+  }
+
+  @DisplayName("상품 수정정보를 입력 받아 상품정보를 수정한다.")
+  @Test
+  void modifyProduct() throws Exception {
+    // given
+    Category category1 = createCategory(null, "냉장");
+    Category category2 = createCategory(category1, "과일");
+    Category category3 = createCategory(category1, "야채");
+    categoryRepository.saveAll(List.of(category1, category2, category3));
+    Product product = createProduct(category2, "충주사과", "thumbnail.jpeg", "detail", 5000, "P001");
+    productRepository.save(product);
+
+    ProductModifyRequest request =
+        new ProductModifyRequest(
+            category3.getId(), "당근", "modified.jpeg", "modifiedDetail", 1000, "P002");
+
+    // when
+    productService.modifyProduct(request, product.getId());
+
+    // then
+    Product getProduct = productRepository.findById(product.getId()).get();
+    assertThat(getProduct)
+        .extracting("name", "thumbnail", "detail", "price", "productCode")
+        .containsExactlyInAnyOrder("당근", "modified.jpeg", "modifiedDetail", 1000, "P002");
+  }
+
+  @DisplayName("존재하지 않는 상품 수정 시 예외가 발생한다.")
+  @Test
+  void modifyProductWithNoExistProductId() throws Exception {
+    // given
+    Category category1 = createCategory(null, "냉장");
+    Category category2 = createCategory(category1, "과일");
+    Category category3 = createCategory(category1, "야채");
+    categoryRepository.saveAll(List.of(category1, category2, category3));
+    Product product = createProduct(category2, "충주사과", "thumbnail.jpeg", "detail", 5000, "P001");
+    productRepository.save(product);
+
+    ProductModifyRequest request =
+        new ProductModifyRequest(
+            category3.getId(), "당근", "modified.jpeg", "modifiedDetail", 1000, "P002");
+
+    Long noExistId = 0L;
+
+    // when // then
+    assertThatThrownBy(() -> productService.modifyProduct(request, noExistId))
+        .isInstanceOf(ProductNotFound.class)
+        .hasMessage("해당 상품이 존재하지 않습니다.");
+  }
+
+  @DisplayName("상품 수정 중 존재하지 않는 카테고리 id인 경우 예외가 발생한다.")
+  @Test
+  void modifyProductWithNoExistCategoryId() throws Exception {
+    // given
+    Category category1 = createCategory(null, "냉장");
+    Category category2 = createCategory(category1, "과일");
+    Category category3 = createCategory(category1, "야채");
+    categoryRepository.saveAll(List.of(category1, category2, category3));
+    Product product = createProduct(category2, "충주사과", "thumbnail.jpeg", "detail", 5000, "P001");
+    productRepository.save(product);
+
+    Long noExistCategoryId = 0L;
+
+    ProductModifyRequest request =
+        new ProductModifyRequest(
+            noExistCategoryId, "당근", "modified.jpeg", "modifiedDetail", 1000, "P002");
+
+    // when // then
+    assertThatThrownBy(() -> productService.modifyProduct(request, product.getId()))
+        .isInstanceOf(CategoryNotFound.class)
+        .hasMessage("해당 카테고리가 존재하지 않습니다.");
+  }
+
+  private Product createProduct(
+      Category category,
+      String name,
+      String thumbnail,
+      String detail,
+      Integer price,
+      String productCode) {
+    return Product.builder()
+        .category(category)
+        .name(name)
+        .thumbnail(thumbnail)
+        .detail(detail)
+        .price(price)
+        .productCode(productCode)
+        .build();
   }
 
   private Category createCategory(Category parent, String name) {
