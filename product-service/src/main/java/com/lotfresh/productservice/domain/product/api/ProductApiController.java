511c60cce25afdb75ad3d163314c37ac21bea0a1
JunYong Sun <ssjy4974@naver.com>
2023-10-04 13:58:14 +0900

    feat : modifyProductApi

--- product-service/src/main/java/com/lotfresh/productservice/domain/product/api/ProductApiController.java
+++ product-service/src/main/java/com/lotfresh/productservice/domain/product/api/ProductApiController.java
@@ -1,14 +1,12 @@
 package com.lotfresh.productservice.domain.product.api;
 
 import com.lotfresh.productservice.domain.product.api.request.ProductCreateRequest;
+import com.lotfresh.productservice.domain.product.api.request.ProductModifyRequest;
 import com.lotfresh.productservice.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,5 +19,12 @@ public class ProductApiController {
   @PostMapping("")
   public ResponseEntity<Long> createProduct(@Valid @RequestBody ProductCreateRequest request) {
     return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(request));
+  }
+
+  @PatchMapping("/{productId}")
+  public ResponseEntity<Void> modifyProduct(
+      @Valid @RequestBody ProductModifyRequest request, @PathVariable("productId") Long productId) {
+    productService.modifyProduct(request, productId);
+    return ResponseEntity.ok().build();
   }
 }
