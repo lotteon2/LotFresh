9e6c52450fcd96906e4555f21d1255f3038cc91a
JunYong Sun <ssjy4974@naver.com>
2023-10-04 10:48:38 +0900

    feat : createProduct

--- product-service/src/main/java/com/lotfresh/productservice/domain/product/service/ProductService.java
+++ product-service/src/main/java/com/lotfresh/productservice/domain/product/service/ProductService.java
@@ -1,5 +1,10 @@
 package com.lotfresh.productservice.domain.product.service;
 
+import com.lotfresh.productservice.domain.category.entity.Category;
+import com.lotfresh.productservice.domain.category.exception.CategoryNotFound;
+import com.lotfresh.productservice.domain.category.repository.CategoryRepository;
+import com.lotfresh.productservice.domain.product.api.request.ProductCreateRequest;
+import com.lotfresh.productservice.domain.product.entity.Product;
 import com.lotfresh.productservice.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @Service
 public class ProductService {
-    private final ProductRepository productRepository;
+  private final ProductRepository productRepository;
+  private final CategoryRepository categoryRepository;
+
+  @Transactional
+  public Long createProduct(ProductCreateRequest request) {
+    Category category =
+        categoryRepository
+            .findById(request.getCategoryId())
+            .orElseThrow(() -> new CategoryNotFound());
+    Product savedProduct = request.toEntity(category);
+    return savedProduct.getId();
+  }
 }
