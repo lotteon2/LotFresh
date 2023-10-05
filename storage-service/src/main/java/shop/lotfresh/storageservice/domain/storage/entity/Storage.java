264b31d3bdd145c7486312acf09794e70c050027
kkh <rlghks131@korea.ac.kr>
2023-10-05 12:40:00 +0900

    test : DB test

---
+++ storage-service/src/main/java/shop/lotfresh/storageservice/domain/storage/entity/Storage.java
@@ -0,0 +1,24 @@
+package shop.lotfresh.storageservice.domain.storage.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Storage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String province;
+
+}
