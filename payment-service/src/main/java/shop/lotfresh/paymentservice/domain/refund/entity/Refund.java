0190236e9da09dfc759cb2f7cd1b5b84796974b5
CessnaJ <eon8748@gmail.com>
2023-10-05 10:52:05 +0900

    :heavy_plus_sign: Add: Refund Entity 추기

--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/entity/Refund.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/entity/Refund.java
@@ -1,4 +1,41 @@
 package shop.lotfresh.paymentservice.domain.refund.entity;
 
-public class Refund {
+import shop.lotfresh.paymentservice.common.BaseTimeEntity;
+import shop.lotfresh.paymentservice.domain.payment.entity.Payment;
+
+import javax.persistence.*;
+
+@Entity
+@Table(
+        name="refund",
+        uniqueConstraints=
+        @UniqueConstraint(columnNames={"productId", "orderId"})
+)
+public class Refund extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false)
+    private String refundReason;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String refundMethod;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Long orderId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Payment payment;
+
+    // TODO: Payment와 양방향 매핑은 필요해지면 작성
 }
