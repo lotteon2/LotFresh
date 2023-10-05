42ee4e78046a8c01a9d5b6f7671da907a761ac5d
CessnaJ <eon8748@gmail.com>
2023-10-05 21:33:51 +0900

    :heavy_plus_sign: Add: RefundService, RefundRepository

--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/repository/RefundRepository.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/repository/RefundRepository.java
@@ -1,4 +1,7 @@
 package shop.lotfresh.paymentservice.domain.refund.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface RefundRepository {
 }
--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/service/RefundService.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/service/RefundService.java
@@ -1,4 +1,18 @@
 package shop.lotfresh.paymentservice.domain.refund.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.lotfresh.paymentservice.domain.refund.api.request.RefundCreateRequest;
+import shop.lotfresh.paymentservice.domain.refund.repository.RefundRepository;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
 public class RefundService {
+    private final RefundRepository refundRepository;
+
+    public Long createRefund(RefundCreateRequest request) {
+
+    }
 }
