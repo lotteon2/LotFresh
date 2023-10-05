a175f2bde65dec2902b1a7e14501a8759e464005
JunYong Sun <ssjy4974@naver.com>
2023-10-05 11:41:34 +0900

    feat : getProductDetailApi

--- product-service/src/main/java/com/lotfresh/productservice/domain/product/api/ProductApiController.java
+++ product-service/src/main/java/com/lotfresh/productservice/domain/product/api/ProductApiController.java
@@ -2,19 +2,25 @@ package com.lotfresh.productservice.domain.product.api;
 
 import com.lotfresh.productservice.domain.product.api.request.ProductCreateRequest;
 import com.lotfresh.productservice.domain.product.api.request.ProductModifyRequest;
+import com.lotfresh.productservice.domain.product.feign.StorageApiClient;
 import com.lotfresh.productservice.domain.product.service.ProductService;
+import com.lotfresh.productservice.domain.product.service.response.ProductResponse;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/products")
 public class ProductApiController {
   private final ProductService productService;
+  private final StorageApiClient storageApiClient;
 
   @PostMapping("")
   public ResponseEntity<Long> createProduct(@Valid @RequestBody ProductCreateRequest request) {
@@ -32,5 +38,18 @@ public class ProductApiController {
   public ResponseEntity<Void> softDeleteProduct(@PathVariable("productId") Long productId) {
     productService.softDelete(productId);
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/{productId}")
+  public ResponseEntity<ProductResponse> getProductDetail(
+      @RequestHeader(value = "userId", required = false) Long userId,
+      @PathVariable("productId") Long productId) {
+    Integer stock = null;
+    try {
+      stock = storageApiClient.getStock(userId, productId);
+    } catch (FeignException e) {
+      log.error(e.getMessage());
+    }
+    return ResponseEntity.ok(productService.getProductDetail(productId, stock));
   }
 }
