b37c80210a143367af1b507c53b90e7f87810ea1
JunYong Sun <ssjy4974@naver.com>
2023-10-05 10:51:34 +0900

    feat : getProductDetail service

--- product-service/src/main/java/com/lotfresh/productservice/domain/product/service/response/ProductResponse.java
+++ product-service/src/main/java/com/lotfresh/productservice/domain/product/service/response/ProductResponse.java
@@ -1,10 +1,12 @@
 package com.lotfresh.productservice.domain.product.service.response;
 
-import lombok.AllArgsConstructor;
+import com.lotfresh.productservice.domain.product.entity.Product;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
-@AllArgsConstructor
+@ToString
 public class ProductResponse {
   private Long id;
   private String name;
@@ -12,7 +14,43 @@ public class ProductResponse {
   private String detail;
   private Integer price;
   private String productCode;
-  private Integer stock;
   private Long categoryId;
   private String categoryName;
+  private Integer stock;
+
+  @Builder
+  public ProductResponse(
+      Long id,
+      String name,
+      String thumbnail,
+      String detail,
+      Integer price,
+      String productCode,
+      Long categoryId,
+      String categoryName,
+      Integer stock) {
+    this.id = id;
+    this.name = name;
+    this.thumbnail = thumbnail;
+    this.detail = detail;
+    this.price = price;
+    this.productCode = productCode;
+    this.categoryId = categoryId;
+    this.categoryName = categoryName;
+    this.stock = stock;
+  }
+
+  public static ProductResponse from(Product product, Integer stock) {
+    return ProductResponse.builder()
+        .id(product.getId())
+        .name(product.getName())
+        .thumbnail(product.getThumbnail())
+        .detail(product.getDetail())
+        .price(product.getPrice())
+        .productCode(product.getProductCode())
+        .categoryId(product.getCategory().getId())
+        .categoryName(product.getCategory().getName())
+        .stock(stock)
+        .build();
+  }
 }
