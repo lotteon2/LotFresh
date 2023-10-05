1ae4543ae9430f4265cf6e7efa6fd91553c28fc6
JunYong Sun <ssjy4974@naver.com>
2023-10-05 12:12:24 +0900

    test : getProductDetailApi test

--- product-service/src/test/java/com/lotfresh/productservice/ControllerTestSupport.java
+++ product-service/src/test/java/com/lotfresh/productservice/ControllerTestSupport.java
@@ -6,13 +6,19 @@ import com.lotfresh.productservice.domain.category.service.CategoryService;
 import com.lotfresh.productservice.domain.discount.api.DiscountApiController;
 import com.lotfresh.productservice.domain.discount.service.DiscountService;
 import com.lotfresh.productservice.domain.product.api.ProductApiController;
+import com.lotfresh.productservice.domain.product.feign.StorageApiClient;
 import com.lotfresh.productservice.domain.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {CategoryApiController.class, DiscountApiController.class, ProductApiController.class})
+@WebMvcTest(
+    controllers = {
+      CategoryApiController.class,
+      DiscountApiController.class,
+      ProductApiController.class
+    })
 public class ControllerTestSupport {
 
   @Autowired protected MockMvc mockMvc;
@@ -24,4 +30,6 @@ public class ControllerTestSupport {
   @MockBean protected DiscountService discountService;
 
   @MockBean protected ProductService productService;
+
+  @MockBean protected StorageApiClient storageApiClient;
 }
