4d920d1c940b5555bd210785ba64053b2075cce2
CessnaJ <eon8748@gmail.com>
2023-10-07 17:02:49 +0900

    :sparkles: Feat: payment entity status변경method 추가, amount -> transactionAmount로 변수명 변경

--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/payment/entity/Payment.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/payment/entity/Payment.java
@@ -28,18 +28,35 @@ public class Payment extends BaseTimeEntity {
     private Long originalAmount;
 
     @Column(nullable = false)
-    private Long amount;
+    private Long transactionAmount;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private String status;
+    private PaymentStatus status;
 
 
-    // order table을 가지고 있지 않음. Relation Mapping을 사용하지 않음.
+    // order table을 가지고 있지 않음. JPA의 Relation Mapping을 사용하지 않음.
     @Column(unique = true,
             nullable = false)
     private Long orderId;
 
     @Column(nullable = false)
     private Long oauthId;
+
+    public void linkPaymentGateway(String pgToken) {
+        this.pgToken= pgToken;
+    }
+
+    public void completePayment() {
+        this.status = PaymentStatus.SUCCESS;
+    }
+
+    public void cancelPayment() {
+        this.status = PaymentStatus.CANCELED;
+    }
+
+    public void failPayment() {
+        this.status = PaymentStatus.FAILED;
+    }
 
 }
