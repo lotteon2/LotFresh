ee19a374a0e87a896fe3ccce6277426bb1035927
CessnaJ <eon8748@gmail.com>
2023-10-07 17:09:32 +0900

    :sparkles: Feat: RefundService - createRefund

--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/service/RefundService.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/service/RefundService.java
@@ -3,7 +3,10 @@ package shop.lotfresh.paymentservice.domain.refund.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.lotfresh.paymentservice.domain.payment.entity.Payment;
+import shop.lotfresh.paymentservice.domain.payment.repository.PaymentRepository;
 import shop.lotfresh.paymentservice.domain.refund.api.request.RefundCreateRequest;
+import shop.lotfresh.paymentservice.domain.refund.entity.Refund;
 import shop.lotfresh.paymentservice.domain.refund.repository.RefundRepository;
 
 @RequiredArgsConstructor
@@ -11,8 +14,15 @@ import shop.lotfresh.paymentservice.domain.refund.repository.RefundRepository;
 @Service
 public class RefundService {
     private final RefundRepository refundRepository;
+    private final PaymentRepository paymentRepository;
 
+    @Transactional
     public Long createRefund(RefundCreateRequest request) {
-
+        Payment payment = paymentRepository.findByOrderId(request.getOrderId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid order ID: " + request.getOrderId()));
+        Refund refund = request.toEntity(payment);
+        Refund createdRefund = refundRepository.save(refund);
+        return createdRefund.getId();
     }
+    
 }
