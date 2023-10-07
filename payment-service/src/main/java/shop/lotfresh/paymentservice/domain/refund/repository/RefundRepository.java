7533e6ee6de86b5793aac6b89fd24795360636a5
CessnaJ <eon8748@gmail.com>
2023-10-07 17:08:42 +0900

    :sparkles: Feat: RefundRepository 추가

--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/repository/RefundRepository.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/repository/RefundRepository.java
@@ -1,7 +1,9 @@
 package shop.lotfresh.paymentservice.domain.refund.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import shop.lotfresh.paymentservice.domain.refund.entity.Refund;
 
 @Repository
-public interface RefundRepository {
+public interface RefundRepository extends JpaRepository<Refund, Long> {
 }
