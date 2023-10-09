1794c46bfcacc2c7ea008f7b6f132d05e16be0f7
qwerty1434 <qwerty1434@naver.com>
2023-10-09 13:52:48 +0900

    :white_check_mark: Test : controller로직 테스트코드 작성

--- order-service/src/main/java/com/lotfresh/orderservice/domain/order/controller/OrderController.java
+++ order-service/src/main/java/com/lotfresh/orderservice/domain/order/controller/OrderController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/order")
@@ -16,7 +18,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PatchMapping("/status")
-    public ResponseEntity changeStatus(@RequestBody OrderDetailChangeStatusRequest orderDetailChangeStatusRequest) {
+    public ResponseEntity changeStatus(@Valid @RequestBody OrderDetailChangeStatusRequest orderDetailChangeStatusRequest) {
         orderService.changeProductOrderStatus(orderDetailChangeStatusRequest);
         return ResponseEntity.ok().build();
     }
--- order-service/src/main/java/com/lotfresh/orderservice/domain/order/controller/request/OrderDetailChangeStatusRequest.java
+++ order-service/src/main/java/com/lotfresh/orderservice/domain/order/controller/request/OrderDetailChangeStatusRequest.java
@@ -6,11 +6,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDetailChangeStatusRequest {
+    @NotNull(message = "orderDetailId cannot be null")
     private Long orderDetailId;
+    @NotNull(message = "orderDetailStatus cannot be null")
     private OrderDetailStatus orderDetailStatus;
 }
