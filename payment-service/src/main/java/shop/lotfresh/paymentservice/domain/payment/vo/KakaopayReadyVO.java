022ffbe751b0435bc05b589bf9a09874512ef52b
CessnaJ <eon8748@gmail.com>
2023-10-10 09:32:41 +0900

    :sparkles: Feat: KakaopayReadyVO 명세 값 추가

--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/payment/vo/KakaopayReadyVO.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/payment/vo/KakaopayReadyVO.java
@@ -1,6 +1,25 @@
 package shop.lotfresh.paymentservice.domain.payment.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class KakaopayReadyVO {
+    private String cid;
 
+    private String partnerOrderId;
+    private String partnerUserId;
 
+    private String itemName;
+    private Long quantity;
+
+    private Long totalAmount;
+    private Long taxFreeAmount;
+
+    private String approvalUrl;
+    private String cancelUrl;
+    private String failUrl;
 }
