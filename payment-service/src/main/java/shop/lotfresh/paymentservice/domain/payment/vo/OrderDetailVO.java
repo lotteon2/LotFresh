f1fcd37ab7076c53e3705a824b566ecabe7477ab
CessnaJ <eon8748@gmail.com>
2023-10-10 21:43:15 +0900

    :sparkles: Feat: OrderDetailVO 작성 - order로부터 받아야할 자료구조

---
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/payment/vo/OrderDetailVO.java
@@ -0,0 +1,24 @@
+package shop.lotfresh.paymentservice.domain.payment.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetailVO {
+    @NotEmpty(message = "productName cannot be Empty")
+    private String productName;
+
+    @Min(value = 0, message = "Price must be equal or greater than 0")
+    private Long price;
+
+    @Min(value = 1, message = "Quantity must be equal or greater than 1")
+    private Long quantity;
+}
