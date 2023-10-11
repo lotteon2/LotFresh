d6690f624101779dd643fc257ca5455610d62526
CessnaJ <eon8748@gmail.com>
2023-10-11 21:46:30 +0900

    :sparkles: Feat: Kakaopay Approve 요청 관련 VO 매핑

--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/payment/vo/KakaopayApproveVO.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/payment/vo/KakaopayApproveVO.java
@@ -3,6 +3,8 @@ package shop.lotfresh.paymentservice.domain.payment.vo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @Builder
 @AllArgsConstructor
@@ -13,4 +15,16 @@ public class KakaopayApproveVO {
     private String partnerOrderId;
     private String partnerUserId;
     private String pgToken;
+
+
+    public MultiValueMap<String, String> toMultiValueMap() {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("cid", cid);
+        map.add("tid", tid);
+        map.add("partner_order_id", partnerOrderId);
+        map.add("partner_user_id", partnerUserId);
+        map.add("pg_token", pgToken);
+
+        return map;
+    }
 }
