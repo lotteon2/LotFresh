de9890f9bf04326c2d734cf0db8fb75b32c5bc38
최창효 <qwerty1434@naver.com>
2023-10-12 19:47:39 +0900

    :sparkles: Feat : N개의 인기상품을 반환하는 쿼리 작성

--- order-service/src/main/java/com/lotfresh/orderservice/domain/order/repository/OrderDetailRepositoryCustomImpl.java
+++ order-service/src/main/java/com/lotfresh/orderservice/domain/order/repository/OrderDetailRepositoryCustomImpl.java
@@ -1,9 +1,9 @@
 package com.lotfresh.orderservice.domain.order.repository;
 
 import com.lotfresh.orderservice.domain.order.entity.OrderDetail;
+import com.lotfresh.orderservice.domain.order.service.response.BestProductsResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -22,9 +22,26 @@ public class OrderDetailRepositoryCustomImpl implements OrderDetailRepositoryCus
         return queryFactory.selectFrom(orderDetail)
                 .where(
                         orderDetail.order.id.eq(orderId),
-                        orderDetail.order.isDeleted.isFalse()
+                        orderDetail.isDeleted.isFalse()
                 )
                 .fetch();
+    }
+
+    @Override
+    public List<BestProductsResponse> mostSoldProducts(int limitCnt) {
+        return queryFactory.select(Projections.constructor(BestProductsResponse.class,
+                orderDetail.productId,
+                orderDetail.id.count())
+                )
+                .from(orderDetail)
+                .where(
+                        orderDetail.isDeleted.isFalse()
+                )
+                .groupBy(orderDetail.productId)
+                .orderBy(orderDetail.id.count().desc())
+                .limit(limitCnt)
+                .fetch();
+
     }
 
 }
