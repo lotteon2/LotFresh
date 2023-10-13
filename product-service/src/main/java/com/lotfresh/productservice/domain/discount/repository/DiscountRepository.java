19f3b81f4a32477c538a8571e111e8412db8e6f9
JunYong Sun <ssjy4974@naver.com>
2023-10-13 09:06:08 +0900

    feat : CommandLineRunner 구현

--- product-service/src/main/java/com/lotfresh/productservice/domain/discount/repository/DiscountRepository.java
+++ product-service/src/main/java/com/lotfresh/productservice/domain/discount/repository/DiscountRepository.java
@@ -5,13 +5,16 @@ import com.lotfresh.productservice.domain.discount.repository.custom.DiscountRep
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface DiscountRepository
     extends JpaRepository<Discount, Long>, DiscountRepositoryCustom {
 
+  @Transactional
   @Modifying(clearAutomatically = true)
   @Query(
       value =
-          "update discount set is_deleted = true where is_deleted = false and end_date <= curdate() + interval 1 day ", nativeQuery = true)
+          "update discount set is_deleted = true where is_deleted = false and end_date <= curdate() + interval 1 day ",
+      nativeQuery = true)
   void updateExpiredDiscounts();
 }
