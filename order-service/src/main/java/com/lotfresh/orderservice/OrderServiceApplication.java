7f1a3a6cfe28a7f12cf1a5723e2c4f82f43d050a
CessnaJ <eon8748@gmail.com>
2023-10-20 21:05:06 +0900

    ❤CI/CD: eureka 등록을 위한 @EnableDiscoveryClient, yml세팅, grdle의존성 추가

--- order-service/src/main/java/com/lotfresh/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/lotfresh/orderservice/OrderServiceApplication.java
@@ -3,7 +3,9 @@ package com.lotfresh.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class OrderServiceApplication {
