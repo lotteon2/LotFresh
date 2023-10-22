8c057407fcef3a026ff5b0119e39eb6e7292d0e7
JunYong Sun <ssjy4974@naver.com>
2023-10-22 19:46:01 +0900

    design : 카테고리 페이지 네이션

--- lotfresh-front/src/views/product/CategoryProductsView.vue
+++ lotfresh-front/src/views/product/CategoryProductsView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container">
+  <div class="SectionTitle st1">
+    <div class="title">
+      <!-- <span class="title_span">신상품</span> -->
+    </div>
     <div class="content">
       <category-product-list />
     </div>
@@ -10,4 +13,32 @@
 import CategoryProductList from "@/components/product/CategoryProductList.vue";
 </script>
 
-<style></style>
+<style scoped>
+.title {
+  padding-top: 50px;
+  padding-bottom: 50px;
+}
+.title_span {
+  color: rgb(51, 51, 51);
+  font-size: 23px;
+  line-height: 1.15;
+  letter-spacing: -0.26px;
+  font-weight: 600;
+  margin-bottom: 30px;
+}
+
+.st1 {
+  margin-bottom: 27px;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+}
+
+.SectionTitle {
+  /* width: 1050px; */
+  margin: auto;
+}
+</style>
