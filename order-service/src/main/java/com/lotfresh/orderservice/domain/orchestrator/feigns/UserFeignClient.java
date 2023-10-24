5228c3495adb6af911d963778195492ca4b0560a
최창효 <qwerty1434@naver.com>
2023-10-24 14:14:05 +0900

    :adhesive_bandage: Fix : @Pathvariable관련 에러 수정

--- order-service/src/main/java/com/lotfresh/orderservice/domain/orchestrator/feigns/UserFeignClient.java
+++ order-service/src/main/java/com/lotfresh/orderservice/domain/orchestrator/feigns/UserFeignClient.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="User",url="localhost:80808081/api/user")
+@FeignClient(name="User",url="localhost:8081/api/user")
 public interface UserFeignClient {
     @GetMapping("/{userId}")
-    String getProvince(@PathVariable Long userId);
+    String getProvince(@PathVariable("userId") Long userId);
 }
