d814ea2a600f56f4ce45234fd6edfc79f6c6beee
JunYong Sun <ssjy4974@naver.com>
2023-10-24 12:29:07 +0900

    feat : 상품리스트 캐시비움 스케줄러 추가 및 서버 시작 시 기능 동작

--- product-service/src/main/java/com/lotfresh/productservice/util/StartupRunner.java
+++ product-service/src/main/java/com/lotfresh/productservice/util/StartupRunner.java
@@ -1,6 +1,7 @@
 package com.lotfresh.productservice.util;
 
 import com.lotfresh.productservice.domain.discount.repository.DiscountRepository;
+import com.lotfresh.productservice.domain.product.scheduler.ProductScheduler;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +12,11 @@ import org.springframework.context.annotation.Profile;
 public class StartupRunner {
 
   @Bean
-  public CommandLineRunner updateExpiredDiscounts(DiscountRepository discountRepository)
+  public CommandLineRunner updateExpiredDiscounts(DiscountRepository discountRepository, ProductScheduler productScheduler)
       throws Exception {
     return args -> {
       discountRepository.updateExpiredDiscounts();
+      productScheduler.clearCache();
     };
   }
 }
