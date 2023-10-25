1a424566c5bdc522d7c0eac39c7e1e0342a5f176
CessnaJ <eon8748@gmail.com>
2023-10-25 14:01:56 +0900

    ✨Feat: 주문서 정보 컴포넌트 재작성중

--- lotfresh-front/src/components/order/orderSheet/OrderProduct.vue
+++ lotfresh-front/src/components/order/orderSheet/OrderProduct.vue
@@ -1,11 +1,78 @@
 <template>
+  <div>
+    <div>
+      <div>주문 상품</div>
+      <div>toggle button</div>
+    </div>
+    <div>
+      <div class="item_info_wrapper">
+        <div class="flex_row align_item_center">
+          <img
+            src="https://product-image.kurly.com/product/image/3df368c8-e124-4d06-a9e9-af4c10d01b53.jpeg"
+            class="item_img"
+          />
+          <div class="content_text">[아임닭] 닭가슴살 꾸이칩 시그니쳐 30g</div>
+        </div>
+        <div class="flex_row">
+          <div class="content_text_light">1개</div>
+          <div class="content_text ml-2vw">2400원</div>
+        </div>
+      </div>
+      <div class="item_info_wrapper">
+        <div class="flex_row align_item_center">
+          <img
+            src="https://product-image.kurly.com/product/image/3df368c8-e124-4d06-a9e9-af4c10d01b53.jpeg"
+            class="item_img"
+          />
+          <div class="content_text">[아임닭] 닭가슴살 꾸이칩 시그니쳐 30g</div>
+        </div>
+        <div class="flex_row">
+          <div class="content_text">1개</div>
+          <div class="content_text">2400원</div>
+        </div>
+      </div>
+    </div>
+    <div class="item_info_wrapper">
+      <div class="flex_row align_item_center">
+        <img
+          src="https://product-image.kurly.com/product/image/3df368c8-e124-4d06-a9e9-af4c10d01b53.jpeg"
+          class="item_img"
+        />
+        <div class="content_text">[아임닭] 닭가슴살 꾸이칩 시그니쳐 30g</div>
+      </div>
+      <div class="flex_row">
+        <div class="content_text">1개</div>
+        <div class="content_text">2400원</div>
+      </div>
+    </div>
+    <div class="item_info_wrapper">
+      <div class="flex_row align_item_center">
+        <img
+          src="https://product-image.kurly.com/product/image/3df368c8-e124-4d06-a9e9-af4c10d01b53.jpeg"
+          class="item_img"
+        />
+        <div class="content_text">[아임닭] 닭가슴살 꾸이칩 시그니쳐 30g</div>
+      </div>
+      <div class="flex_row">
+        <div class="content_text">1개</div>
+        <div class="content_text">2400원</div>
+      </div>
+    </div>
+  </div>
+
   <div class="order-product-container">
     <div class="demo-collapse">
       <el-collapse>
-        <el-collapse-item name="1">
+        <el-collapse-item name="1" class="is-active">
           <template #title>
             <div class="title"><h2>주문 상품</h2></div>
           </template>
+          <div class="item">
+            mounted시점에 redis로 axios통신을 해서 구매상품을 가져온다
+          </div>
+          <div class="item">
+            mounted시점에 redis로 axios통신을 해서 구매상품을 가져온다
+          </div>
           <div class="item">
             mounted시점에 redis로 axios통신을 해서 구매상품을 가져온다
           </div>
@@ -28,5 +95,46 @@ export default {
 .order-product-container {
   margin-right: auto;
   margin-left: auto;
+}
+
+.item_img {
+  width: 8vw;
+  height: 8vw;
+  object-fit: cover;
+  margin-right: 2vw;
+  border-radius: 10px;
+}
+
+.item_info_wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 3vh;
+}
+
+.flex_row {
+  display: flex;
+  flex-direction: row;
+}
+
+.align_item_center {
+  align-items: center;
+}
+
+.content_text {
+  font-size: 1rem;
+  font-weight: bold;
+  line-height: 1.1rem;
+}
+
+.ml-2vw {
+  margin-left: 1.5vw;
+}
+
+.content_text_light {
+  font-size: 0.9rem;
+  font-weight: normal;
+  line-height: 1.1rem;
 }
 </style>
--- lotfresh-front/src/views/OrderSheetView.vue
+++ lotfresh-front/src/views/OrderSheetView.vue
@@ -10,9 +10,7 @@
     <orderer-info />
     <delivery-info />
     <payment-bill />
-    <div class="pay-button">
-      <button>카카오페이로 결제하기</button>
-    </div>
+    <KakaopayButton @kakaopay_button_click="handlePayment"></KakaopayButton>
   </div>
 </template>
 
@@ -21,8 +19,58 @@ import DeliveryInfo from "../components/order/orderSheet/DeliveryInfo.vue";
 import OrdererInfo from "../components/order/orderSheet/OrdererInfo.vue";
 import OrderProduct from "../components/order/orderSheet/OrderProduct.vue";
 import PaymentBill from "../components/order/orderSheet/PaymentBill.vue";
+import KakaopayButton from "../components/order/orderSheet/KakaopayButton.vue";
+import type { OrderCreateRequest } from "../api/order/order";
+import { startKakaopay } from "../api/order/order";
+
 export default {
-  components: { DeliveryInfo, OrdererInfo, OrderProduct, PaymentBill },
+  components: {
+    DeliveryInfo,
+    OrdererInfo,
+    OrderProduct,
+    PaymentBill,
+    KakaopayButton,
+  },
+
+  setup() {
+    const handlePayment = async () => {
+      try {
+        const orderData: OrderCreateRequest = {
+          productRequests: [
+            {
+              productId: 1,
+              productPrice: 10000,
+              productStock: 50,
+              productName: "Product Name",
+              productThumbnail: "https://example.com/product-thumbnail.jpg",
+            },
+            {
+              productId: 2,
+              productPrice: 20000,
+              productStock: 30,
+              productName: "Another Product Name",
+              productThumbnail:
+                "https://example.com/another-product-thumbnail.jpg",
+            },
+            // 필요한 만큼 추가적인 ProductRequest 객체를 포함시킬 수 있습니다.
+          ],
+          isFromCart: false, // 장바구니에서 주문하는 경우 true, 그렇지 않으면 false
+        };
+        const res = await startKakaopay(orderData);
+        res ? (window.location.href = res) : console.log("없거나 실패");
+        // if (result.status === 200) {
+        //   window.location.href = result.data.body;
+        // } else {
+        //   console.log("실패했습니다");
+        //   alert("실패");
+        // }
+      } catch (error) {
+        console.error(error);
+        alert("오류가 발생했습니다: " + error);
+      }
+    };
+    return { handlePayment };
+  },
 };
 </script>
 
