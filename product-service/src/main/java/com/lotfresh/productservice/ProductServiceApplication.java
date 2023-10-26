fe6e30597ddf11ed0af207deea339be8b61b7c81
CessnaJ <eon8748@gmail.com>
2023-10-26 09:26:04 +0900

    ❤CI/CD: 빌드 테스트

--- product-service/src/main/java/com/lotfresh/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/lotfresh/productservice/ProductServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
-// 
+
   public static void main(String[] args) {
     SpringApplication.run(ProductServiceApplication.class, args);
   }
