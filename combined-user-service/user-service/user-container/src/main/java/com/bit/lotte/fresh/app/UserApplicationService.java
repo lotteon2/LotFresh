05606b93070ad775d3f58072cdd158fca03b9dd7
CessnaJ <eon8748@gmail.com>
2023-10-26 13:52:00 +0900

    💥Chore: auth, user서버 yml 재작성

--- combined-user-service/user-service/user-container/src/main/java/com/bit/lotte/fresh/app/UserApplicationService.java
+++ combined-user-service/user-service/user-container/src/main/java/com/bit/lotte/fresh/app/UserApplicationService.java
@@ -3,6 +3,7 @@ package com.bit.lotte.fresh.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -16,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "com.bit.lotte.fresh.data.user.adapter")
 @ComponentScan(basePackages = "com.bit.lotte.fresh.data.user.mapper")
 @ComponentScan(basePackages = "com.bit.lotte.fresh.message.publish")
+@EnableDiscoveryClient
 public class UserApplicationService {
 
   public static void main(String[] args) {
