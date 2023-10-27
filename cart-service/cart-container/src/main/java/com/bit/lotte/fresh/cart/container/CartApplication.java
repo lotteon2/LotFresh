a429a0aa6c6c8095a92bea8c3e2f6327e34d8c3e
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-10-27 14:25:35 +0900

    add: add the eruka  dependency

--- cart-service/cart-container/src/main/java/com/bit/lotte/fresh/cart/container/CartApplication.java
+++ cart-service/cart-container/src/main/java/com/bit/lotte/fresh/cart/container/CartApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableDiscoveryClient
 @EnableJpaRepositories(basePackages = {"com.bit.lotte.fresh.cart.data.repository"})
 @EntityScan(basePackages = {"com.bit.lotte.fresh.cart.data.entity"})
 @ComponentScan("com.bit.lotte.fresh.cart.service")
