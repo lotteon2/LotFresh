00f0538773b2d394379d11e76a568a197ed6854d
CessnaJ <eon8748@gmail.com>
2023-10-29 01:51:48 +0900

    ❤CI/CD: 통합테스트 빌드

--- storage-service/src/main/java/shop/lotfresh/storageservice/StorageServiceApplication.java
+++ storage-service/src/main/java/shop/lotfresh/storageservice/StorageServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class StorageServiceApplication {
 
 	public static void main(String[] args) {
-		
+		// 
 		SpringApplication.run(StorageServiceApplication.class, args);
 	}
 
