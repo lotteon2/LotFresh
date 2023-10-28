8da09746bde3d3c9108c0ef2b4e5411803c852d8
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-10-28 16:50:23 +0900

    fix: fix the kakao redirect url

--- auth-service/auth-application/src/main/java/com/bit/lotte/fresh/auth/valueobject/Secret.java
+++ auth-service/auth-application/src/main/java/com/bit/lotte/fresh/auth/valueobject/Secret.java
@@ -2,6 +2,6 @@ package com.bit.lotte.fresh.auth.valueobject;
 
 public class Secret {
   public static String KAKAO_REST_KEY ="5dca3ee52a5c5e81b0415473b05366f0";
-  public static String KAKAO_REDIRECT ="https://www.lot-fresh-shop/auth/oauth/kakao/login";
+  public static String KAKAO_REDIRECT ="https://www.lot-fresh-shop/auth-service/auth/oauth/kakao/login";
   public static String LOGIN_REQUEST_URI ="https://www.lot-fresh-shop/auth/login";
 }
