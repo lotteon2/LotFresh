1b7beac7b9fd5a2e52de005293a2875a1bc8aeab
JunYong Sun <ssjy4974@naver.com>
2023-10-30 12:21:22 +0900

    feat : 카트 담기 기능 구현

--- lotfresh-front/src/api/utils/index.ts
+++ lotfresh-front/src/api/utils/index.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 const BASE_URL = "https://www.lot-fresh.shop/";
 const PRODUCT_URL = "https://www.lot-fresh.shop/product-service/";
 const ORDER_URL = "http://localhost:8084";
+const CART_URL = "https://www.lot-fresh.shop/cart-service/";
 
 // 기본 API 요청 처리
 const axiosApi = (baseURL: string) => {
@@ -16,3 +17,4 @@ const axiosApi = (baseURL: string) => {
 export const defaultInstance = axiosApi(BASE_URL);
 export const productInstance = axiosApi(PRODUCT_URL);
 export const orderInstance = axiosApi(ORDER_URL);
+export const cartInstance = axiosApi(CART_URL);
