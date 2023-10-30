b6480a6e78c5f62376fa65b411f1b80724c7fb44
kkh <rlghks131@korea.ac.kr>
2023-10-30 12:38:33 +0900

    feat : lotfresh-admin pushtest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from "./components/SignIn";
+import Main from "./components/Main";
+import Storages from './components/Storages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/storages" element={<Storages />} />
+      </Routes>
+    </Router>
   );
 }
 
