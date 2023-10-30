6d2cd8c29a109a2c3fc60449580795d8e36cb0de
CessnaJ <eon8748@gmail.com>
2023-10-30 17:58:00 +0900

    ❤CI/CD: storage-service 유레카에 등록

--- storage-service/src/main/java/shop/lotfresh/storageservice/StorageServiceApplication.java
+++ storage-service/src/main/java/shop/lotfresh/storageservice/StorageServiceApplication.java
@@ -3,7 +3,10 @@ package shop.lotfresh.storageservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableTransactionManagement
 public class StorageServiceApplication {
