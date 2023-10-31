21b09f5af4cfe4653fa00468f1b4ac5231ddccf8
CessnaJ <eon8748@gmail.com>
2023-10-31 16:32:34 +0900

    😥CI/CD: 기존 auth-user combined 디렉토리 삭제

--- combined-user-service/auth-service/auth-application/src/main/java/com/bit/lotte/fresh/auth/valueobject/OauthProviderURI.java
+++
@@ -1,9 +0,0 @@
-package com.bit.lotte.fresh.auth.valueobject;
-
-public class OauthProviderURI {
-
-  public static String KAKAO_AUTHORIZATION_CODE = "https://kauth.kakao.com/oauth/token";
-  public static String KAKAO_USER_INFO = "https://kauth.kakao.com/user/me";
-
-
-}
--- combined-user-service/auth-service/auth-domain/auth-domain-core/src/main/java/com/bit/lotte/fresh/auth/valueobject/AuthRole.java
+++
@@ -1,8 +0,0 @@
-package com.bit.lotte.fresh.auth.valueobject;
-
-public enum AuthRole {
-  ROLE_SYSTEM_ADMIN,
-  ROLE_USER,
-  ROLE_NOT_VERIFIED,
-  ROLE_CATEGORY_ADMIN;
-}
