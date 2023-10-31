28f942e40540d611858441ef195df126ff8618c0
CessnaJ <eon8748@gmail.com>
2023-10-31 16:22:14 +0900

    ✨Feat: payment- 관리자의 환불 조회기능

--- payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/repository/RefundRepository.java
+++ payment-service/src/main/java/shop/lotfresh/paymentservice/domain/refund/repository/RefundRepository.java
@@ -1,10 +1,13 @@
 package shop.lotfresh.paymentservice.domain.refund.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import shop.lotfresh.paymentservice.domain.refund.entity.Refund;
+import shop.lotfresh.paymentservice.domain.refund.entity.RefundStatus;
 
 @Repository
 public interface RefundRepository extends JpaRepository<Refund, Long> {
@@ -13,4 +16,6 @@ public interface RefundRepository extends JpaRepository<Refund, Long> {
     // TODO: QueryDSL 통해 타입세이프한 구현필요
     @Query("SELECT SUM(r.amount) FROM Refund r WHERE r.payment.id = :paymentId AND r.status = 'APPROVED'")
     Long findTotalApprovedRefundedAmountByPaymentId(@Param("paymentId") Long paymentId);
+
+    Page<Refund> findByStatus(RefundStatus status, Pageable pageable);
 }
