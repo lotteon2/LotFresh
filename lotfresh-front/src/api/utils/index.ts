97b695792aad31f7bab7c82f545168b1f3c30e84
JunYong Sun <ssjy4974@naver.com>
2023-10-31 11:12:57 +0900

    fix : product detail api 변경에 따른 수정

--- lotfresh-front/src/api/utils/index.ts
+++ lotfresh-front/src/api/utils/index.ts
@@ -5,7 +5,8 @@ const BASE_URL = "https://www.lot-fresh.shop/";
 const PRODUCT_URL = "https://www.lot-fresh.shop/product-service/";
 // const PRODUCT_URL = "http://localhost:8083/";
 // const ORDER_URL = "http://localhost:8084";
-const MEMBER_URL = "http://localhost:8081";
+// const MEMBER_URL = "http://localhost:8081";
+const MEMBER_URL = "https://www.lot-fresh.shop/member-service/";
 const ORDER_URL = "https://www.lot-fresh.shop/order-service/";
 const CART_URL = "https://www.lot-fresh.shop/cart-service/";
 
