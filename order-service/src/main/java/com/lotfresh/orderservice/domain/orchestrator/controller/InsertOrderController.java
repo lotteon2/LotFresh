11c3ad91cd7ea29ab9cf6f17ac8eaad3609f57cf
최창효 <qwerty1434@naver.com>
2023-11-02 11:01:05 +0900

    :pencil2: Fix : redirect 주소 수정

--- order-service/src/main/java/com/lotfresh/orderservice/domain/orchestrator/controller/InsertOrderController.java
+++ order-service/src/main/java/com/lotfresh/orderservice/domain/orchestrator/controller/InsertOrderController.java
@@ -35,7 +35,7 @@ public class InsertOrderController {
             @PathVariable(name ="province") String province, @PathVariable(name="isBargain") Boolean isBargain,
             @RequestParam String pg_token, HttpServletResponse response) throws IOException {
         orchestratorService.doTransaction(userId, province, pg_token, orderId, isFromCart ,isBargain);
-        response.sendRedirect("https://www.lot-fresh.shop/orderdetail/"+orderId);
+        response.sendRedirect("https://www.lot-fresh.shop/payment-result/success/"+orderId);
         return ResponseEntity.ok().build();
     }
 
