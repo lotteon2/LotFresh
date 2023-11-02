4824347a0e06340d765f74b6e20ce37db41ef17c
최창효 <qwerty1434@naver.com>
2023-11-02 15:35:26 +0900

    :pencil2: Fix : 상태변경 로직에 누락된 @Transactional 수정

--- order-service/src/main/java/com/lotfresh/orderservice/domain/orchestrator/process/workflow/step/orderStep/PaymentStatusChanger.java
+++ order-service/src/main/java/com/lotfresh/orderservice/domain/orchestrator/process/workflow/step/orderStep/PaymentStatusChanger.java
@@ -5,6 +5,7 @@ import com.lotfresh.orderservice.domain.order.entity.status.PaymentStatus;
 import com.lotfresh.orderservice.domain.order.repository.OrderDetailRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 public class PaymentStatusChanger {
     private final OrderDetailRepository orderDetailRepository;
 
+    @Transactional
     public void changeOrderDetailStatus(Long orderId, PaymentStatus status) {
         List<OrderDetail> orderDetails = orderDetailRepository.findAllByOrderId(orderId);
         orderDetails.forEach(orderdetail -> orderdetail.changePaymentStatus(status));
