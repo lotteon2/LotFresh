5ac7244938ce8d62475c780c791cd586f0e09214
JunYong Sun <ssjy4974@naver.com>
2023-11-02 20:06:47 +0900

    fix : api url 수정

--- lotfresh-front/src/stores/product.ts
+++ lotfresh-front/src/stores/product.ts
@@ -5,7 +5,7 @@ import type { RecentProducts } from "@/interface/productInterface";
 export const useProductStore = defineStore("product", () => {
   const recentProducts = ref<RecentProducts[]>([]);
   const setRecentProducts = (id: number, thumbnail: string) => {
-    if (recentProducts.value.length === 50) {
+    if (recentProducts.value.length === 20) {
       recentProducts.value.shift();
       recentProducts.value.push({ id, thumbnail });
     } else {
