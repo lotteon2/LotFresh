8b16d8076f8e6dacdd9a9d5dbf63182e18a67319
최창효 <qwerty1434@naver.com>
2023-11-02 11:50:20 +0900

    :pencil2: Fix : alert정보 수정

--- lotfresh-front/src/components/cart/PaymentSide.vue
+++ lotfresh-front/src/components/cart/PaymentSide.vue
@@ -78,7 +78,7 @@ export default {
         .catch((err) => {
           console.log(err);
         });
-      alert(JSON.stringify(this.items));
+      alert(JSON.stringify(this.orderSheetList));
     },
   },
 };
